3e23bc2f1b4fc1ba5109e9f1a4967ed91ee88f87
Lucas Magalhães Rodrigues <127453899+iamlucasmagalhaes@users.noreply.github.com>
2026-02-09 10:26:38 -0300

    feat: setup react-router-dom and basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <h1>GitHub Finder</h1>
+      <Outlet />
     </div>
   );
 }
